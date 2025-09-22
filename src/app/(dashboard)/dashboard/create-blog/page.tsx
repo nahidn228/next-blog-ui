31a507149ff4c937a96245ad9dbd27f12bdf44c5
nahidn228 <nahidn228@gmail.com>
2025-09-22 23:27:46 +0600

    Creating Blogs Using Next.js Server Actions

--- src/app/(dashboard)/dashboard/create-blog/page.tsx
+++ src/app/(dashboard)/dashboard/create-blog/page.tsx
@@ -1,9 +1,11 @@
+import CreateBlogForm from "@/components/modules/Blogs/CreateBlogForm";
 import React from "react";
 
 const CreateBlog = () => {
   return (
-    <div>
+    <div className="w-full justify-center items-center mx-auto">
       <h1 className="text-center text-xl">Create Blog</h1>
+      <CreateBlogForm />
     </div>
   );
 };
