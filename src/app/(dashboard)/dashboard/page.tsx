9780a9af41c6fe876e64f9c60df7a4b596fe93b9
nahidn228 <nahidn228@gmail.com>
2025-09-25 22:33:30 +0600

    Access session user & logout safely

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,18 @@
-import { authOptions } from "@/helpers/authOptions";
-import { getServerSession } from "next-auth";
+// import { authOptions } from "@/helpers/authOptions";
+// import { getServerSession } from "next-auth";
+import { getUserSession } from "@/helpers/getUserSession";
 
 const DashboardHomePage = async () => {
-  const session = await getServerSession(authOptions);
+  const session = await getUserSession();
+  // const session = await getServerSession(authOptions);
   console.log(session);
   return (
-    <div>
-      <h1>Dashboard Home Page</h1>
+    <div className="min-h-screen flex items-center justify-center p-6 w-full">
+      <div>
+        <h1>Welcome to Dashboard</h1>
+        <h2>Hey {session?.user?.name}, Welcome Back</h2>
+        <span>{session?.user?.email}</span>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import AuthProvider from "@/providers/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,14 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
