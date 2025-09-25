bf1f4415c8904eab38e1790a02962f4c8d6a9356
nahidn228 <nahidn228@gmail.com>
2025-09-25 20:11:45 +0600

    get user info when login with OAuth using auth_secret

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,9 @@
-const DashboardHomePage = () => {
+import { authOptions } from "@/helpers/authOptions";
+import { getServerSession } from "next-auth";
+
+const DashboardHomePage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div>
       <h1>Dashboard Home Page</h1>
