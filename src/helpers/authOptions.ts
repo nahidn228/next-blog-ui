b36a11b1e3974323fe77cf19efe8d605044f1b15
nahidn228 <nahidn228@gmail.com>
2025-09-27 21:20:51 +0600

    Implement NextAuth Credentials login system

--- src/helpers/authOptions.ts
+++ src/helpers/authOptions.ts
@@ -1,10 +1,67 @@
 import GoogleProvider from "next-auth/providers/google";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.password || !credentials.email) {
+          console.error("Email or Password is missing");
+          return null;
+        }
+
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_API}/auth/login`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email: credentials.email,
+                password: credentials.password,
+              }),
+            }
+          );
+          if (!res?.ok) {
+            console.error("User Login Failed", await res.text());
+            return null;
+          }
+
+          const user = await res.json();
+
+          if (user) {
+            return {
+              id: user?.id,
+              name: user?.name,
+              email: user?.email,
+              role: user?.role,
+              phone: user?.phone,
+              picture: user?.picture,
+              status: user?.status,
+              isVerified: user?.isVerified,
+              createdAt: user?.createdAt,
+              updatedAt: user?.updatedAt,
+            };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      },
     }),
   ],
   secret: process.env.AUTH_SECRET,
